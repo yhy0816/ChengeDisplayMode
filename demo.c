be878a360bba923684e45c2c5805de4550737d40
于洪洋 <97099566+yhy0816@users.noreply.github.com>
2023-11-22 23:09:45 +0800

    Update demo.c

--- demo.c
+++ demo.c
@@ -5,42 +5,30 @@
 
 
 int main(){
-
+    FreeConsole();
+    FILE* file = freopen("ChangeMode.log", "a", stdout);
     SYSTEM_POWER_STATUS power_status;
-    while(1){
 
-        if (GetSystemPowerStatus(&power_status)) {
+    while(GetSystemPowerStatus(&power_status)){
 
-            if (power_status.ACLineStatus == 1) {
-                printf("电源供电\n");
-
-            } else if (power_status.ACLineStatus == 0) {
-                printf("电池供电\n");
-                if(system("DisplaySwitch.exe /internal") == 0){
+        if(power_status.ACLineStatus == 0) { // 如果是电池供电就尝试切换
+            for (int i = 0; i < 3; i++) {
+                if (system("DisplaySwitch.exe /internal") == 0) {
 
                     printf("切换成功\n");
-                    return 0;
+                    break;
 
-                }else{
-                    printf("切换失败\n");
-                    return -1;
+                } else {
+                    printf("第%d次切换失败\n", i);
+
                 }
-            } else {
-                printf("error\n");
-                return -1;
             }
-
-
-
-
-        } else {
-            printf("error\n");
-            return 0;
+            break; // 三次切换失败退出程序
         }
+
         sleep(2);
-
     }
-
+    fclose(file);
     return 0;
 
 
